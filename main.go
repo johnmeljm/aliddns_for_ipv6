694c4b79dd7f28c4e1fc9dd7e3ea77f5f312ccd8
johnmel <johnmeljm@hotmail.com>
2024-11-21 15:59:11 +0800

    rename config file

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	var configFile string
-	flag.StringVar(&configFile, "c", "./config/config.yaml", "config file")
+	flag.StringVar(&configFile, "c", "./aliddns.yaml", "config file")
 	flag.Parse()
 	Init(configFile)
 	aliddns()
